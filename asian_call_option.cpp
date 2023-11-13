c670ec58929bd0eda4443187f17c96f13f5713e0
thomascourtney <64978864+thomascourtney@users.noreply.github.com>
2023-11-13 13:29:16 -0500

    changed several aspects

--- asian_call_option.cpp
+++ asian_call_option.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
 class AsianCallOption {
 private:
@@ -17,32 +18,54 @@ public:
     AsianCallOption(double S, double K, double r, double sigma, double T, int paths, int steps)
         : spotPrice(S), strikePrice(K), riskFreeRate(r), volatility(sigma), timeToMaturity(T), numPaths(paths), numSteps(steps) {}
 
-    // Function to generate a random number between 0 and 1
     double rand01() {
         return rand() / (double)RAND_MAX;
     }
+    // Function to generate a vector of random numbers
+    std::vector<double> generateRandomNumbers(int count) {
+        std::vector<double> randomNumbers;
+        for (int i = 0; i < count; ++i) {
+            randomNumbers.push_back(rand01());
+        }
+        return randomNumbers;
+    }
 
-    // Monte Carlo simulation for Asian call option pricing
+    // Monte Carlo simulation for Asian call option pricing using antithetic variance reduction
     double price() {
         double dt = timeToMaturity / numSteps;
         double sumPayoff = 0.0;
         srand(static_cast<unsigned>(time(0)));  // Seed for random number generation
 
         for (int i = 0; i < numPaths; ++i) {
-            double spotPriceCopy = spotPrice;
-            double sumSpotPrice = spotPrice;
-
-            for (int j = 1; j < numSteps; ++j) {
-                double epsilon = rand01();
-                double drift = riskFreeRate - 0.5 * volatility * volatility;
-                double diffusion = volatility * sqrt(dt) * epsilon;
-
-                spotPriceCopy *= exp(drift * dt + diffusion);
-                sumSpotPrice += spotPriceCopy;
+            // Generate two sets of independent random numbers using antithetic variates
+            std::vector<double> epsilon1 = generateRandomNumbers(numSteps);
+            std::vector<double> epsilon2 = epsilon1;  // Antithetic variates
+            for (double& e : epsilon2) {
+                e = 1.0 - e;
             }
 
-            double averageSpotPrice = sumSpotPrice / numSteps;
-            double payoff = fmax(averageSpotPrice - strikePrice, 0.0);  // Asian call option payoff
+            double spotPriceCopy = spotPrice;
+            double sumSpotPrice1 = spotPrice;
+            double sumSpotPrice2 = spotPrice;
+
+            for (int j = 1; j < numSteps; ++j) {
+                double diffusion1 = volatility * sqrt(dt) * epsilon1[j - 1];
+                double diffusion2 = volatility * sqrt(dt) * epsilon2[j - 1];
+
+                // Apply antithetic variates to the diffusion term
+                spotPriceCopy *= exp((riskFreeRate - 0.5 * volatility * volatility) * dt + 0.5 * (diffusion1 + diffusion2));
+                sumSpotPrice1 += spotPriceCopy;
+                sumSpotPrice2 += spotPriceCopy;
+            }
+
+            double averageSpotPrice1 = sumSpotPrice1 / numSteps;
+            double averageSpotPrice2 = sumSpotPrice2 / numSteps;
+
+            double payoff1 = fmax(averageSpotPrice1 - strikePrice, 0.0);
+            double payoff2 = fmax(averageSpotPrice2 - strikePrice, 0.0);
+
+            // Apply antithetic variates to the payoff
+            double payoff = 0.5 * (payoff1 + payoff2);
 
             sumPayoff += payoff;
         }
@@ -53,12 +76,12 @@ public:
 
 int main() {
     // Example parameters
-    double spotPrice = 100.0;
-    double strikePrice = 100.0;
-    double riskFreeRate = 0.05;
-    double volatility = 0.2;
-    double timeToMaturity = 1.0;
-    int numPaths = 100000;
+    double spotPrice = 200.0;
+    double strikePrice = 220.0;
+    double riskFreeRate = 0.06;
+    double volatility = 0.1;
+    double timeToMaturity = 2.0;
+    int numPaths = 200000;
     int numSteps = 252;
 
     // Create an instance of the AsianCallOption class
